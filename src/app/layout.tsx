68970857f3f1417ce93ded80530bb8c81db1135f
joevics100 <joevics100@gmail.com>
2026-07-08 23:27:34 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+import { seoConfig } from "@/config/seo.config";
+import { env } from "@/env";
+
+import { Footer, Header } from "@/components/layout";
+import { Toaster } from "@/ui";
+import { Providers } from "@/providers";
+
+import "@/tailwind";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  ...seoConfig
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen w-full flex-col antialiased`}
+      >
+        <Providers>
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Toaster richColors />
+          <Footer />
+        </Providers>
+
+        {env.NEXT_PUBLIC_GA_ID && <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />}
+      </body>
+    </html>
+  );
+}
